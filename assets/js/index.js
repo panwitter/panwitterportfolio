02217755fa0bca60688792c71bec23a93ca26097
няшк! <92618443+Byfi38@users.noreply.github.com>
2024-01-27 18:11:01 +1000

    ЧТО ЭТО ЧЁ ПОМИДОР

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,39 @@
-if ("1" == 1) {
-    
-} else {
-    
-}
-for (let i = 0; i < 10; i++) {
-    console.log(i);  
-}
-let a = 10
-while (a > 0) {
-    console.log(a)
-    a--
+let work = document.getElementById("work");
+let education = document.getElementById("education");
+let list = document.querySelector(".page-about_profile-list")
+
+console.log(work,education,list);
+
+function click() {
+    console.log("нажали");
 }
 
+work.onclick = function () {
+    console.log("нажали на work");
+    list.innerHTML = "";
+    list.insertAdjacentHTML("afterbegin",` <article class="profile-item">
+              <h5>помидор</h5>
+              <div class="profile-item_bottom">
+                <h5>ЧТО ЭТО ЧЁ ПОМИДОР</h5>
+                <div class="profile-item_date">
+                  <img src="./assets/img/calendar.svg" alt="" />
+                  <h6>2009-2014</h6>
+                </div>
+              </div>
+            </article>`)
+};
+
+education.onclick = () => {
+    console.log("нажали на education");
+    list.innerHTML = "";
+    list.insertAdjacentHTML("afterbegin",` <article class="profile-item">
+              <h5>хлеб</h5>
+              <div class="profile-item_bottom">
+                <h5>Я НЕ МОГУ ПОНЯТЬ</h5>
+                <div class="profile-item_date">
+                  <img src="./assets/img/calendar.svg" alt="" />
+                  <h6>2009-2014</h6>
+                </div>
+              </div>
+            </article>`)
+}
