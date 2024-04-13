5fd1e390db7af063aeb70b8a83948289476a8939
картон по жизни <92618443+panwitter@users.noreply.github.com>
2024-04-13 18:27:45 +1000

    пэинт вау

--- whatahell/index.js
+++ whatahell/index.js
@@ -1,11 +1,41 @@
-let blocks = document.querySelectorAll(".block");
-console.log(blocks);
+let wrapper = document.querySelector(".bigbox");
+const blockWidth = 20;
+let s = (wrapper.offsetWidth * wrapper.offsetHeight) / Math.pow(blockWidth,2);
+console.log(s);
 
-for (let i = 0; i < blocks.length; i++) {
-  blocks[i].addEventListener("mouseenter", function () {
-    blocks[i].classList.add("--active");
-    setTimeout(() => {
-      blocks[i].classList.remove("--active");
-    }, 1000);
-  });
+for (let i = 0; i<s; i++) {
+  wrapper.insertAdjacentHTML("afterbegin", '<div class="block"></div>');
 }
+
+let color = document.getElementById("color");
+console.log(color.value);
+
+let blocksList = document.querySelectorAll(".block");
+
+
+let isDraw = false
+
+wrapper.addEventListener("mousedown", () => {
+  isDraw = true;
+})
+
+wrapper.addEventListener("mouseup", () => {
+  isDraw = false;
+})
+
+blocksList.forEach((block) => {
+  block.addEventListener("mouseenter", () => {
+    if (isDraw) {
+      block.style.background = color.value;
+    }
+  });
+});
+
+// for (let i = 0; i < blocks.length; i++) {
+//   blocks[i].addEventListener("mouseenter", function () {
+//     blocks[i].classList.add("--active");
+//     setTimeout(() => {
+//       blocks[i].classList.remove("--active");
+//     }, 1000);
+//   });
+// }
